f350fe2df9cd5d76b5a3489e45811cb512775775
yuvalg04 <yuvalgeva04@gmail.com>
2023-01-22 12:50:19 +0200

    fix other mistakes

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,95 +1,152 @@
 import java.util.Scanner;
 
 public class Connect4 {
-    static char[][] board = new char[6][7];
-    static Scanner sc = new Scanner(System.in);
+    private char[][] grid;
+    private int turn;
+    private char player;
+    private boolean winner;
+    private Scanner in;
+
+    public Connect4() {
+        grid = new char[6][7];
+        turn = 1;
+        player = 'R';
+        winner = false;
+        in = new Scanner(System.in);
+
+        //initialize array
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                grid[row][col] = ' ';
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        initBoard();
-        while (true) {
-            printBoard();
-            playerMove(1);
-            if (checkWin(1)) {
-                System.out.println("Player 1 wins!");
-                break;
+        Connect4 game = new Connect4();
+        game.play();
+    }
+
+
+    public void play() {
+        while (!winner && turn <= 42) {
+            boolean validPlay;
+            int play;
+            do {
+                display();
+
+                System.out.print("Player " + player + ", choose a column: ");
+                play = in.nextInt();
+
+                //validate play
+                validPlay = validate(play);
+
+            } while (!validPlay);
+
+            //drop the checker
+            for (int row = grid.length - 1; row >= 0; row--) {
+                if (grid[row][play] == ' ') {
+                    grid[row][play] = player;
+                    break;
+                }
             }
-            printBoard();
-            playerMove(2);
-            if (checkWin(2)) {
-                System.out.println("Player 2 wins!");
-                break;
+
+            //determine if there is a winner
+            winner = isWinner();
+
+            //switch players
+            if (player == 'R') {
+                player = 'B';
+            } else {
+                player = 'R';
             }
+
+            turn++;
+        }
+        display();
+
+        if (winner) {
+            if (player == 'R') {
+                System.out.println("Black won");
+            } else {
+                System.out.println("Red won");
+            }
+        } else {
+            System.out.println("It's a tie");
         }
     }
 
-    public static void initBoard() {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 7; j++) {
-                board[i][j] = '.';
-            }
-        }
-    }
-
-    public static void printBoard() {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 7; j++) {
-                System.out.print(board[i][j] + " ");
+    private void display() {
+        System.out.println(" 0 1 2 3 4 5 6");
+        System.out.println("---------------");
+        for (int row = 0; row < grid.length; row++) {
+            System.out.print("|");
+            for (int col = 0; col < grid[0].length; col++) {
+                System.out.print(grid[row][col]);
+                System.out.print("|");
             }
             System.out.println();
+            System.out.println("---------------");
         }
+        System.out.println(" 0 1 2 3 4 5 6");
+        System.out.println();
     }
 
-    public static void playerMove(int player) {
-        System.out.print("Player " + player + ", choose a column: ");
-        int col = sc.nextInt();
-        while(board[0][col] != '.'){
-            System.out.println("Column is full. Choose another column.");
-            col = sc.nextInt();
+    private boolean validate(int column) {
+        //valid column?
+        if (column < 0 || column > grid[0].length) {
+            return false;
         }
-        for (int i = 5; i >= 0; i--) {
-            if (board[i][col] == '.') {
-                if (player == 1) {
-                    board[i][col] = 'X';
-                } else {
-                    board[i][col] = 'O';
-                }
-                break;
-            }
+
+        //full column?
+        if (grid[0][column] != ' ') {
+            return false;
         }
+
+        return true;
     }
 
-
-    public static boolean checkWin(int player) {
-        char marker = 'X';
-        if (player == 2) {
-            marker = 'O';
-        }
-
-        // check horizontal
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (board[i][j] == marker && board[i][j+1] == marker && board[i][j+2] == marker && board[i][j+3] == marker) {
+    private boolean isWinner() {
+        //check for 4 across
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length - 3; col++) {
+                if (grid[row][col] == player &&
+                        grid[row][col + 1] == player &&
+                        grid[row][col + 2] == player &&
+                        grid[row][col + 3] == player) {
                     return true;
                 }
             }
         }
-
-        // check vertical
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 7; j++) {
-                if (board[i][j] == marker && board[i+1][j] == marker && board[i+2][j] == marker && board[i+3][j] == marker) {
+        //check for 4 up and down
+        for (int row = 0; row < grid.length - 3; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == player &&
+                        grid[row + 1][col] == player &&
+                        grid[row + 2][col] == player &&
+                        grid[row + 3][col] == player) {
                     return true;
                 }
             }
         }
-
-        // check diagonal
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (board[i][j] == marker && board[i+1][j+1] == marker && board[i+2][j+2] == marker && board[i+3][j+3] == marker) {
+        //check upward diagonal
+        for (int row = 3; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length - 3; col++) {
+                if (grid[row][col] == player &&
+                        grid[row - 1][col + 1] == player &&
+                        grid[row - 2][col + 2] == player &&
+                        grid[row - 3][col + 3] == player) {
                     return true;
                 }
-                if (board [i][j+3] == marker && board[i+1][j+2] == marker && board[i+2][j+1] == marker && board[i+3][j] == marker) {
+            }
+        }
+        //check downward diagonal
+        for (int row = 0; row < grid.length - 3; row++) {
+            for (int col = 0; col < grid[0].length - 3; col++) {
+                if (grid[row][col] == player &&
+                        grid[row + 1][col + 1] == player &&
+                        grid[row + 2][col + 2] == player &&
+                        grid[row + 3][col + 3] == player) {
                     return true;
                 }
             }
